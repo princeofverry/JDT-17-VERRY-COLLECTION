d70a4eef698efa82061f1db81454dfe1bbee0700
princeofverry <verryk49@gmail.com>
2026-06-02 09:20:01 +0700

    feat: mutable list

---
+++ src/main/java/com/indivaragroup/list/exam/MutableList.java
@@ -0,0 +1,25 @@
+package com.indivaragroup.list.exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MutableList {
+
+    public static void printData(List data) {
+        data.add("data tidak sengaja");
+        System.out.println("Di dalam method: " + data);
+    }
+
+    public static void main(String[] args) {
+        ArrayList data = new ArrayList<>();
+        data.add("Budi");
+        data.add("Ari");
+
+        // tambah method
+        System.out.println("Sebelum method dipanggil : " + data); // budi, ari
+
+        printData(data); // budi, ari, data tidak sengaja
+
+        System.out.println("Setelah method dipanggil : " + data); // budi, ari, data tidak disengaja
+    }
+}
